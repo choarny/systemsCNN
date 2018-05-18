bf521b7bed922397c10d85a756b6bc87183b1340
Ryan Cho <cho.ryan212@gmail.com>
2018-05-18 19:19:31 -0400

    Added skeleton, compile

--- CNN.py
+++ CNN.py
@@ -2,6 +2,8 @@
 File Name: CNN
 Author: Ryan Cho
 Implementation of Keras and Tensorflow
+
+LeNet-5 Architecture
 """
 from __future__ import print_function
 import keras
@@ -16,3 +18,21 @@ epochs = 10
 
 # input image dimensions
 img_x, img_y = 28, 28
+
+#LeNet-5
+model = Sequential()
+model.add(Conv3D(32, kernel_size=(5, 5), strides=(1, 1),
+                 activation='relu',
+                 input_shape=None))
+model.add(MaxPooling3D())
+model.add(Conv2D()
+model.add(MaxPooling3D())
+model.add(Flatten())
+model.add(Dense())
+model.add(Dense(num_classes, activation='softmax'))
+
+#compile
+model.compile(optimizer=keras.optimizers.SGD(lr=0.01),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
