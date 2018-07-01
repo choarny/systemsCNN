266b948f83f839ca5d0bcf8156d70670292c5e5c
Ryan Cho <cho.ryan212@gmail.com>
2018-06-30 22:04:37 -0400

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,6 +1,6 @@
 import random
 import os
-import urllib.request
+import urllib
 
 if not os.path.exists('training'):
     os.makedirs('training')
@@ -8,7 +8,7 @@ if not os.path.exists('test'):
     os.makedirs('test')
 
 #training data
-os.system("cd training")
+#os.system("cd training")
 for i in range(10):
     #tiling_resolution = random.randint(0, 11)
     tiling_resolution = 11
@@ -17,11 +17,11 @@ for i in range(10):
     #column_num = random.randint(200, 400)
     #row_num = random.randint(400, 800)
     url = str("https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/Landsat_WELD_NDVI_Global_Monthly/default/2010-04-01/31.25m/" + str(tiling_resolution) + "/" + str(column_num) + "/" + str(row_num) + ".jpg")
-    urllib.request.urlretrieve(url, str(i) + ".jpg")
+    urllib.urlretrieve(url, str(i) + ".jpg")
 
-os.system("cd test")
+#os.system("cd test")
 #test data
-for i in range(10000):
+for i in range(10):
     tiling_resolution = 11
     column_num = 320
     row_num = 640
@@ -29,4 +29,4 @@ for i in range(10000):
     #column_num = random.randint(200, 400)
     #row_num = random.randint(400, 800)
     url = str("https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/Landsat_WELD_NDVI_Global_Monthly/default/2010-04-01/31.25m/" + str(tiling_resolution) + "/" + str(column_num) + "/" + str(row_num) + ".jpg")
-    urllib.request.urlretrieve(url, str(i) + ".jpg")
+    urllib.urlretrieve(url, str(i) + ".jpg")
