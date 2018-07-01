50d528de8ce0b01a6222667c7497617437c46b8e
Ryan Cho <cho.ryan212@gmail.com>
2018-06-30 20:58:30 -0400

    Update Generator.py

--- CNN.py
+++ CNN.py
@@ -7,6 +7,7 @@ LeNet-5 Architecture
 """
 from __future__ import print_function
 import keras
+import #the dataset
 from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
@@ -19,10 +20,14 @@ epochs = 10
 # input image dimensions
 img_x, img_y = 28, 28
 
+x_train = x_train.reshape(x_train.shape[0], img_x, img_y, 3)
+x_test = x_test.reshape(x_test,shape[0], img_x, img_y, 3)
+input_shape = (img_x, img_y, 3)
+
 #LeNet-5
 model = Sequential()
 model.add(Conv3D(32, kernel_size=(5, 5), strides=(1, 1),
-                 activation=relu',
+                 activation='relu',
                  input_shape=None))
 model.add(MaxPooling3D())
 model.add(Conv3D()
--- Generator.py
+++ Generator.py
@@ -1,18 +1,32 @@
-"""
-Training and Test Data Generator
-"""
 import random
-import urllib.request
 import os
-if not os.path.exists("training"):
-    os.makedirs("training")
-if not os.path.exists("test"):
-    os.makedirs("test") 
+import urllib.request
+
+if not os.path.exists('training'):
+    os.makedirs('training')
+if not os.path.exists('test'):
+    os.makedirs('test')
 
 #training data
+os.system("cd training")
 for i in range(10):
+    #tiling_resolution = random.randint(0, 11)
     tiling_resolution = 11
-    column_num = random.randint(200, 400)
-    row_num = random.randint(400, 800)
+    column_num = 320
+    row_num = 640
+    #column_num = random.randint(200, 400)
+    #row_num = random.randint(400, 800)
+    url = str("https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/Landsat_WELD_NDVI_Global_Monthly/default/2010-04-01/31.25m/" + str(tiling_resolution) + "/" + str(column_num) + "/" + str(row_num) + ".jpg")
+    urllib.request.urlretrieve(url, str(i) + ".jpg")
+
+os.system("cd test")
+#test data
+for i in range(10000):
+    tiling_resolution = 11
+    column_num = 320
+    row_num = 640
+    #tiling_resolution = random.randint(0, 11)
+    #column_num = random.randint(200, 400)
+    #row_num = random.randint(400, 800)
     url = str("https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/Landsat_WELD_NDVI_Global_Monthly/default/2010-04-01/31.25m/" + str(tiling_resolution) + "/" + str(column_num) + "/" + str(row_num) + ".jpg")
     urllib.request.urlretrieve(url, str(i) + ".jpg")
